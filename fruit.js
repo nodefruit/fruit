7d3eb7a5e2a33d02b9101452c8938a39d66308ed
Khalid REHIOUI <Rehioui@gmail.com>
2015-10-20 07:40:24 +0100

    Code cleaning

--- fruit.js
+++ fruit.js
@@ -4,30 +4,31 @@ module.exports = (function () {
   var defer = require('./lib/defer');
   
   function Fruit (adapter) {
+    
     var _adapter = adapter;
     
     function getCallBack (deferred) {
       return function (err, result) {
         if(err) deferred.reject(err);
-        else    deferred.resolve(result);
+        else deferred.resolve(result);
       }
     }
     
     this.insert = function (tocName, data) {
       var deferred = defer();
-      _adapter.insert(tocName, data, getCallBack (deferred))
+      _adapter.insert(tocName, data, getCallBack(deferred))
       return deferred.getPromise();
     }
     
     this.find = function (tocName, condition) {
       var deferred = defer();
-      _adapter.find(tocName, condition, getCallBack (deferred));
+      _adapter.find(tocName, condition, getCallBack(deferred));
       return deferred.getPromise();
     }
     
     this.findAll = function (tocName) {
       var deferred = defer();
-      _adapter.findAll(tocName, getCallBack (deferred));
+      _adapter.findAll(tocName, getCallBack(deferred));
       return deferred.getPromise();
     }
     
