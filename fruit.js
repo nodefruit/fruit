125c71b116360c483afb944bc8c89c1cfbd772df
Khalid REHIOUI <Rehioui@gmail.com>
2015-10-20 07:33:10 +0100

    Using Q for promises

--- fruit.js
+++ fruit.js
@@ -1,13 +1,40 @@
 /* بسم الله الرحمن الرحيم */
 
 module.exports = (function () {
+  var defer = require('./lib/defer');
   
   function Fruit (adapter) {
-    this.insert   = adapter.insert
-    this.find     = adapter.find
-    this.findAll  = adapter.findAll
+    var _adapter = adapter;
+    
+    function getCallBack (deferred) {
+      return function (err, result) {
+        if(err) deferred.reject(err);
+        else    deferred.resolve(result);
+      }
+    }
+    
+    this.insert = function (tocName, data) {
+      var deferred = defer();
+      _adapter.insert(tocName, data, getCallBack (deferred))
+      return deferred.getPromise();
+    }
+    
+    this.find = function (tocName, condition) {
+      var deferred = defer();
+      _adapter.find(tocName, condition, getCallBack (deferred));
+      return deferred.getPromise();
+    }
+    
+    this.findAll = function (tocName) {
+      var deferred = defer();
+      _adapter.findAll(tocName, getCallBack (deferred));
+      return deferred.getPromise();
+    }
+    
     this.update   = adapter.update
+    
     this.remove   = adapter.remove
+    
   }
   
   return Fruit;
